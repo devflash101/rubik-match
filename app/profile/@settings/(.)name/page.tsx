01a5cb64c3fdf591ddc83e3d03a11b2f77e41d35
devflash101 <devflash101@gmail.com>
2025-04-21 07:32:49 -0400

    intercepting routes for modal
    Co-authored-by: amilajack <amilajack@gmail.com>

---
+++ app/profile/@settings/(.)name/page.tsx
@@ -0,0 +1,10 @@
+import { Modal } from "./modal";
+
+export default function ProfileSettingsName() {
+    return (
+        <div className="modal">
+            <h1>This is Profile Settings Name Page</h1>
+        </div>
+        // <Modal>This is Profile Settings Name Page</Modal>
+    )
+};
