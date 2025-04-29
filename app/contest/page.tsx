e4cc1b63091839e57ad28ddafdc1a6ea9181e24c
devflash101 <devflash101@gmail.com>
2025-04-28 22:22:58 -0400

    using route
    Co-authored-by: amilajack <amilajack@gmail.com>

--- app/contest/page.tsx
+++ app/contest/page.tsx
@@ -1,14 +1,18 @@
-'use client';
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+// 'use client';
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
 
-const ContestPage = () => {
-    const [contestType, setContestType] = useState(0);
-    const router = useRouter();
+const ContestPage = async () => {
+    // const [contestType, setContestType] = useState(0);
+    // const router = useRouter();
+    const res = await fetch('/api', { cache: 'no-store' });
+    const json = await res.json();
+
     return (
         <div>
-            <div>This is Contest {contestType} Page</div>
-            <button onClick={() => router.push('/match/multi/4*4')}>Match</button>
+            {/* <div>This is Contest {contestType} Page</div> */}
+            {/* <button onClick={() => router.push('/match/multi/4*4')}>Match</button> */}
+            <p>API Response: {json}</p>
         </div>
     )
 };
