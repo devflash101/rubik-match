fd1406102483f973d86800819a6c87f1e4da1b16
devflash101 <devflash101@gmail.com>
2025-05-27 18:43:18 -0700

    URL Query Parameters
    Co-authored-by: Rongronggg9 <i@rong.moe>

--- app/api/match/[type]/route.ts
+++ app/api/match/[type]/route.ts
@@ -1,9 +1,14 @@
+import { type NextRequest } from 'next/server'
+
 export async function GET(request:Request,
     {params} : { params: Promise<{type: string}> }
 ) {
     const { type } = await params;
-    return new Response(`${type} match Api endpoint`, {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query');
+
+    return new Response(`${type} match Api endpoint with ${query}`, {
         status: 200,
     })
-    // api/match/3_3
+    // api/match/3_3?query=hello
 }
